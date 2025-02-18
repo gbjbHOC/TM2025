bff31729e43d76c3a977d2b2c3ba6361fc0f441c
Patrick Hochuli <patrick.hochuli@gbjb.ch>
2025-02-18 08:55:40 +0100

    "esai de février"

---
+++ 03c_2nb.py
@@ -0,0 +1,4 @@
+n1=int(input('Donnez un nombre       : '))
+n2=int(input('Donnez un autre nombre : '))
+
+print(n1," + ",n2," = ",n1+n2," et ",n1," * ",n2," = ",n1*n2)
--- essai.py
+++ essai.py
@@ -1,1 +1,2 @@
 print("coucou tout le monde he he")
+print("Autre essai")
